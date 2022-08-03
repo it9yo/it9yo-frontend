4782bc701b412302ebbfdb8ce20c0e572461f463
zu21un <jwjoo03@gmail.com>
2022-08-03 16:45:42 +0900

    fix: prettier 규칙 수정

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,16 +1,15 @@
 module.exports = {
-  arrowParens: 'avoid',
+  arrowParens: 'always'
+  bracketSpacing: true
   bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-
-  jsxBracketSameLine: false,
-  jsxSingleQuote: false,
-  printWidth: 80,
-  proseWrap: 'always',
-  quoteProps: 'as-needed',
-  semi: true,
-  tabWidth: 2,
-  trailingComma: 'es5',
-  useTabs: false,
+  jsxBracketSameLine: false
+  jsxSingleQuote: false
+  printWidth: 80
+  proseWrap: 'always'
+  quoteProps: 'as-needed'
+  semi: true
+  singleQuote: true
+  tabWidth: 2
+  trailingComma: 'es5'
+  useTabs: false
 };
