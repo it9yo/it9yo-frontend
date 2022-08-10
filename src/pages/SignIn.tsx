d6bc12555c13f7cc19f5d58abe0c836ea9cb8d66
zu21un <jwjoo03@gmail.com>
2022-08-10 15:34:32 +0900

    feat: react-native-config 적용, .env를 통해 환경 변수 설정

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -5,11 +5,16 @@ import {
   Text,
   View,
   Image,
+  Platform,
+  Alert,
 } from 'react-native';
+
+import Config from 'react-native-config';
+import axios, { AxiosError } from 'axios';
 
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import {
-  getProfile, KakaoOAuthToken, KakaoProfile, login, logout, unlink,
+  getProfile, KakaoOAuthToken, KakaoProfile, login,
 } from '@react-native-seoul/kakao-login';
 import { RootStackParamList } from '../../App';
 
@@ -21,38 +26,49 @@ import GoogleBtn from '../assets/images/googleBtn.png';
 
 type SignInScreenProps = NativeStackScreenProps<RootStackParamList, 'SignUp'>;
 
-function SignIn({ navigation }: SignInScreenProps) {
-  const [result, setResult] = useState();
+interface userAuthenticationProps {
+  id: string;
+  ProviderType: string;
+}
 
+function SignIn({ navigation }: SignInScreenProps) {
   const toSignUp = useCallback(() => {
     navigation.navigate('SignUp');
   }, [navigation]);
 
   const signInWithKakao = async (): Promise<void> => {
     const token: KakaoOAuthToken = await login();
-    console.log(`kakao token ${JSON.stringify(token)}`);
+    // console.log(`kakao token ${JSON.stringify(token)}`);
     const profile: KakaoProfile = await getProfile();
-    console.log(`kakao profile ${JSON.stringify(profile)}`);
-    // setResult(JSON.stringify(token));
+    // console.log(`kakao profile ${JSON.stringify(profile)}`);
+    const { id } = profile;
+    console.log(id);
+    const userProps: userAuthenticationProps = {
+      id,
+      ProviderType: 'KAKAO',
+    };
   };
 
-  const signOutWithKakao = async (): Promise<void> => {
-    const message = await logout();
-
-    setResult(message);
-  };
-
-  const getKakaoProfile = async (): Promise<void> => {
-    const profile: KakaoProfile = await getProfile();
-
-    setResult(JSON.stringify(profile));
-  };
-
-  const unlinkKakao = async (): Promise<void> => {
-    const message = await unlink();
-
-    setResult(message);
-  };
+  const authenticateUser = useCallback(({ id, ProviderType }: userAuthenticationProps) => {
+    try {
+      const response = await axios.post(
+        `${
+          Platform.OS === 'ios' ? Config.API_URL_IOS : Config.API_URL_ANDROID
+        }/user/auth/login`,
+        {
+          id,
+          ProviderType,
+        },
+      );
+      console.log(response.data);
+      // TODO: 회원가입 여부 체크
+    } catch (error) {
+      const errorResponse = (error as AxiosError).response;
+      if (errorResponse) {
+        Alert.alert('알림', errorResponse.data.message);
+      }
+    }
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -70,7 +86,7 @@ function SignIn({ navigation }: SignInScreenProps) {
           <Image style={styles.loginButtonLogo}
             source={NaverBtn}
           />
-          <Text style={styles.loginButtonText}>네이버로 회원가입</Text>
+          <Text style={styles.loginButtonText}>네이버로 계속하기</Text>
         </Pressable>
         <Pressable
           style={styles.loginButton}
@@ -79,7 +95,7 @@ function SignIn({ navigation }: SignInScreenProps) {
           <Image style={styles.loginButtonLogo}
             source={KakaoBtn}
           />
-          <Text style={styles.loginButtonText}>카카오로 회원가입</Text>
+          <Text style={styles.loginButtonText}>카카오로 계속하기</Text>
         </Pressable>
         <Pressable
           style={styles.loginButton}
@@ -88,15 +104,7 @@ function SignIn({ navigation }: SignInScreenProps) {
           <Image style={styles.loginButtonLogo}
             source={GoogleBtn}
           />
-          <Text style={styles.loginButtonText}>구글로 회원가입</Text>
-        </Pressable>
-      </View>
-      <View style={styles.buttonZone}>
-        <Pressable
-          style={styles.loginButton}
-          onPress={toSignUp}
-        >
-          <Text style={styles.loginButtonText}>로그인</Text>
+          <Text style={styles.loginButtonText}>구글로 계속하기</Text>
         </Pressable>
       </View>
     </View>
