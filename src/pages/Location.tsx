937f7b1615d0da834d91ae138320f34a70370212
zu21un <jwjoo03@gmail.com>
2022-08-16 13:30:07 +0900

    refactor: recoil 적용을 위한 AppInner 추가, 지역 인증 페이지 추가

---
+++ src/pages/Location.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function Location() {
+  return (
+    <View>
+      <Text>지역 입력 받기</Text>
+    </View>
+  );
+}
+
+export default Location;
