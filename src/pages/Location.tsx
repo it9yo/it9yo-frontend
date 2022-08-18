767c25d94b4c5f3301b8bc21638ef8212da5ad47
zu21un <jwjoo03@gmail.com>
2022-08-18 18:19:42 +0900

    feat: 위치 설정 페이지 추가

--- src/pages/Location.tsx
+++ src/pages/Location.tsx
@@ -1,12 +1,69 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import React, { useState } from 'react';
+import {
+  StyleSheet, Text, TouchableOpacity, View,
+} from 'react-native';
+import { RootStackParamList } from '../@types';
+import address from '../constants/address';
 
-function Location() {
+type Props = NativeStackScreenProps<RootStackParamList, 'Location'>;
+
+function Location({ navigation }: Props) {
+  const [sido, setSido] = useState<string | null>(null);
+  const [sigungu, setSigungu] = useState<string | null>(null);
+
+  const sidoList = Object.keys(address);
+
   return (
-    <View>
-      <Text>지역 입력 받기</Text>
+    <View style={styles.container}>
+      <View style={styles.textList}>
+      {sidoList.map((item, idx) => (
+        <TouchableOpacity
+          key={idx}
+          onPress={() => setSido(item)}>
+          <Text style={item === sido
+            ? styles.selectedText
+            : styles.commonText}>
+            {item}
+          </Text>
+        </TouchableOpacity>
+      ))}
+      </View>
+      <View style={styles.textList}>
+        {sido !== null && address[sido].map((item, idx) => (
+          <TouchableOpacity
+          key={idx}
+          onPress={() => setSigungu(item)}>
+          <Text style={item === sigungu
+            ? styles.selectedText
+            : styles.commonText}>
+            {item}
+          </Text>
+        </TouchableOpacity>
+        ))}
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  textList:
+  {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  selectedText: {
+    fontWeight: 'bold',
+    textDecoration: 'underline',
+  },
+  commonText: {
+
+  },
+});
 
 export default Location;
