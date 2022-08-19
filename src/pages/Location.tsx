f36490a67b895ce3259a28085ea0b8a2e5552e6e
zu21un <jwjoo03@gmail.com>
2022-08-19 15:05:29 +0900

    feat: 위치 설정 페이지 하단 버튼 추가

--- src/pages/Location.tsx
+++ src/pages/Location.tsx
@@ -1,6 +1,8 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React, { useState } from 'react';
 import {
+  Button,
+  Pressable,
   StyleSheet, Text, TouchableOpacity, View,
 } from 'react-native';
 import { RootStackParamList } from '../@types';
@@ -14,15 +16,27 @@ function Location({ navigation }: Props) {
 
   const sidoList = Object.keys(address);
 
+  const handleSido = (item) => {
+    setSido(item);
+    console.log(typeof item);
+    if (item === '세종특별자치시') {
+      setSigungu('');
+    } else {
+      setSigungu(null);
+    }
+  };
+
+  const canGoNext = sido && sigungu !== null;
+
   return (
     <View style={styles.container}>
       <View style={styles.textList}>
       {sidoList.map((item, idx) => (
         <TouchableOpacity
           key={idx}
-          onPress={() => setSido(item)}>
+          onPress={() => { handleSido(item); }}>
           <Text style={item === sido
-            ? styles.selectedText
+            ? StyleSheet.compose(styles.commonText, styles.selectedText)
             : styles.commonText}>
             {item}
           </Text>
@@ -35,12 +49,27 @@ function Location({ navigation }: Props) {
           key={idx}
           onPress={() => setSigungu(item)}>
           <Text style={item === sigungu
-            ? styles.selectedText
+            ? StyleSheet.compose(styles.commonText, styles.selectedText)
             : styles.commonText}>
             {item}
           </Text>
         </TouchableOpacity>
         ))}
+      </View>
+
+      <View style={styles.buttonZone}>
+        <TouchableOpacity style={StyleSheet.compose(styles.button, styles.buttonActive)}>
+          <Text style={styles.buttonText}>이  전</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={
+            canGoNext
+              ? StyleSheet.compose(styles.button, styles.buttonActive)
+              : styles.button
+          }
+          disabled={!canGoNext}>
+          <Text style={styles.buttonText}>지역 인증 하기</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -48,21 +77,49 @@ function Location({ navigation }: Props) {
 
 const styles = StyleSheet.create({
   container: {
+    height: '100%',
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     flexDirection: 'row',
   },
-  textList:
-  {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  selectedText: {
-    fontWeight: 'bold',
-    textDecoration: 'underline',
+  textList: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
   },
   commonText: {
-
+    marginTop: 8,
+    paddingLeft: 12,
+    color: 'gray',
+    fontSize: 20,
+  },
+  selectedText: {
+    color: 'black',
+    fontWeight: 'bold',
+  },
+  buttonZone: {
+    position: 'absolute',
+    width: '90%',
+    bottom: 30,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginHorizontal: 5,
+  },
+  button: {
+    width: '45%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'gray',
+    paddingVertical: 10,
+    borderRadius: 5,
+  },
+  buttonActive: {
+    backgroundColor: 'black',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
   },
 });
 
