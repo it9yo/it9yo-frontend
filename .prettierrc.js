2392d1d65054caa4a57ab104f8e022881e9bcd29
Hwang-Sangjin <aosdb5640@naver.com>
2022-08-24 16:09:11 +0900

    Campaign Register temp

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,15 +1,15 @@
 module.exports = {
-  arrowParens: 'always'
-  bracketSpacing: true
+  arrowParens: 'always',
+  bracketSpacing: true,
   bracketSameLine: true,
-  jsxBracketSameLine: false
-  jsxSingleQuote: false
-  printWidth: 80
-  proseWrap: 'always'
-  quoteProps: 'as-needed'
-  semi: true
-  singleQuote: true
-  tabWidth: 2
-  trailingComma: 'es5'
-  useTabs: false
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  proseWrap: 'always',
+  quoteProps: 'as-needed',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
 };
