b2afb0c8995f9e79bc45a23e7c98174e8685b610
zu21un <jwjoo03@gmail.com>
2022-11-07 16:13:17 +0900

    fix: 회원가입시 전화번호 확인 안되는 버그 수정

---
+++ src/pages/Chat/ChatModal.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function ChatModal() {
+  return <View>
+    <Text>hello</Text>
+  </View>;
+}
+
+export default ChatModal;
