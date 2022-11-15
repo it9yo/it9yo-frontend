a9dd3ba182699ab24d16b17cadbe66f8a8ef06bb
zu21un <jwjoo03@gmail.com>
2022-11-16 01:07:59 +0900

    fix: 채팅 리스트 ui 수정

--- src/components/EachChat.tsx
+++ src/components/EachChat.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Image, Pressable, StyleSheet, Text, View,
 } from 'react-native';
@@ -6,29 +6,70 @@ import {
 import { useNavigation } from '@react-navigation/native';
 import StatusNameList from '@constants/statusname';
 import { ChatRoomData } from '@src/@types';
+import AsyncStorage from '@react-native-community/async-storage';
+import { IMessage } from 'react-native-gifted-chat';
+
+function timeConversion(millisec: number) {
+  const seconds = Math.floor(millisec / 1000);
+
+  const minutes = Math.floor(millisec / (1000 * 60));
+
+  const hours = Math.floor(millisec / (1000 * 60 * 60));
+
+  const days = Math.floor(millisec / (1000 * 60 * 60 * 24));
+
+  if (Number(hours) >= 24) {
+    return `${days}일`;
+  } if (Number(minutes) >= 60) {
+    return `${hours}시간`;
+  } if (Number(seconds) >= 60) {
+    return `${minutes}분`;
+  }
+  return `${seconds}초`;
+}
 
 function EachChat({ item } : { item: ChatRoomData }) {
   const navigation = useNavigation();
+  const [lastChat, setLastChat] = useState('');
+  const [lastTime, setLastTime] = useState('');
 
   const {
-    campaignId, title, itemImageURLs, campaignStatus, participatedPersonCnt,
+    campaignId, title, itemImageURLs,
   } = item;
 
+  useEffect(() => {
+    getLastMessage();
+  }, []);
+
+  const getLastMessage = async () => {
+    const prevMessages = await AsyncStorage.getItem(`chatMessages_${campaignId}`);
+    if (!prevMessages) return;
+    const messageList: IMessage[] = JSON.parse(prevMessages);
+    const recentMessage = messageList[0];
+    const { text, createdAt } = recentMessage;
+    console.log(createdAt, typeof createdAt);
+    setLastChat(text);
+    const lastTimeText = `${timeConversion(new Date().getTime() - new Date(createdAt).getTime())} 전`;
+    setLastTime(lastTimeText);
+  };
   return <Pressable onPress={() => navigation.navigate('ChatRoom', { campaignId, title })}>
     <View style={styles.chatListView}>
-      <Image style={styles.chatThumbnail}
-        source={{
-          uri: itemImageURLs[0],
-        }}
-      />
-      <View>
-        <Text style={styles.chatTitle}>{title}</Text>
-        {/* <Text style={styles.chatContent}>{chatContent}</Text> */}
+      <View style={styles.leftContainer}>
+        <Image style={styles.chatThumbnail}
+          source={{
+            uri: itemImageURLs[0],
+          }}
+        />
+        <View style={styles.chatContainer}>
+          <Text style={styles.chatTitle}>{title}</Text>
+          <Text style={styles.chatContent}>{lastChat}</Text>
+        </View>
       </View>
-      <View style={styles.chatStateView}>
-        {/* <Text>{chatTime}</Text> */}
-        <Text style={styles.campaignStatusText}>{StatusNameList[campaignStatus]}</Text>
-        <Text style={styles.joinedPeopleText}>{`${participatedPersonCnt}명 참여중`}</Text>
+      <View style={styles.rightContainer}>
+        <Text style={styles.chatTimeText}>{lastTime}</Text>
+        {/* <View style={styles.badge}>
+          <Text style={styles.badgeNum}>1</Text>
+        </View> */}
       </View>
     </View>
   </Pressable>;
@@ -37,44 +78,70 @@ const styles = StyleSheet.create({
   chatListView: {
     flexDirection: 'row',
     backgroundColor: '#fff',
-    paddingHorizontal: 10,
-    paddingVertical: 15,
+    paddingHorizontal: 25,
+    paddingVertical: 20,
     borderBottomWidth: 1,
-    borderColor: 'white',
+    borderColor: '#f5f5f5',
+    justifyContent: 'space-between',
+  },
+  leftContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   chatThumbnail: {
-    width: 50,
-    height: 50,
-    borderRadius: 50 / 2,
-    marginRight: 10,
+    width: 55,
+    height: 55,
+    borderRadius: 10,
+  },
+  chatContainer: {
+    marginLeft: 20,
   },
   chatTitle: {
-    fontSize: 18,
-    marginBottom: 10,
-    fontWeight: '400',
-    color: 'black',
+    fontFamily: 'NotoSansKR',
+    fontSize: 17,
+    fontWeight: 'bold',
+    fontStyle: 'normal',
+    letterSpacing: -0.38,
+    color: '#404040',
+    marginBottom: 2,
   },
   chatContent: {
-    fontSize: 16,
-    marginBottom: 5,
-    fontWeight: '400',
-    color: 'gray',
+    fontFamily: 'NotoSansKR',
+    fontSize: 15,
+    fontWeight: 'normal',
+    fontStyle: 'normal',
+    letterSpacing: -0.33,
+    color: '#404040',
   },
-  chatStateView: {
+  rightContainer: {
+    justifyContent: 'flex-start',
     alignItems: 'flex-end',
-    fontFamily: 'Proxima Nova',
-    position: 'absolute',
-    top: 15,
-    right: 15,
+    marginTop: 5,
+  },
+  chatTimeText: {
+    fontFamily: 'NotoSansKR',
     fontSize: 14,
+    fontWeight: 'normal',
+    fontStyle: 'normal',
+    letterSpacing: -0.3,
+    color: '#9a9a9a',
+    marginBottom: 5,
   },
-  campaignStatusText: {
-    color: 'red',
-    paddingTop: 5,
+  badge: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 20,
+    paddingHorizontal: 6,
+    borderRadius: 12,
+    backgroundColor: '#fb5135',
   },
-  joinedPeopleText: {
-    color: 'orange',
-    paddingTop: 5,
+  badgeNum: {
+    fontFamily: 'Roboto',
+    fontSize: 12,
+    fontWeight: '500',
+    fontStyle: 'normal',
+    letterSpacing: -0.3,
+    color: '#ffffff',
   },
 });
 
